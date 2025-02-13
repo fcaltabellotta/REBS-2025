fbc2bd35174821d19e66a3b38c817099bd4e0de9
shcaba <jason.cope@noaa.gov>
2025-02-13 13:04:41 -0800

    update index figures

--- docs/Pre_assessment/plots/Data comp plots.R
+++ docs/Pre_assessment/plots/Data comp plots.R
@@ -49,4 +49,26 @@ ggplot(Catch_comp_in_All,aes(Year,Landings,color=as.factor(Assessment)))+
 ##Compare Indices ##
 ####################
 
+Indices_comp_in<-read.csv("C:/Users/Jason.Cope/Documents/Github/REBS-2025/docs/Pre_assessment/plots/Indices_comp.csv")
+Indices_comp_in_WCGBTS<-subset(Indices_comp_in,Survey=="WCGBTS")
+Indices_comp_in_NWFSC<-subset(Indices_comp_in,Survey=="NWFSC_slope")
+Indices_comp_in_WCGBTS<-subset(Indices_comp_in,Survey=="WCGBTS")
+Indices_comp_in_WCGBTS<-subset(Indices_comp_in,Survey=="WCGBTS")
 
+ggplot(Indices_comp_in_WCGBTS,aes(as.numeric(Year),as.numeric(Index),color=as.factor(Type)))+
+  geom_point()
+  
+ggplot(Indices_comp_in_NWFSC,aes(as.numeric(Year),as.numeric(Index),color=as.factor(Type)))+
+  geom_point(position=position_dodge(width=0.5))+
+  ylim(0,3000)
+
+
+ggplot(Indices_comp_in,aes(as.numeric(Year),as.numeric(Index),color=as.factor(Type)))+
+  geom_point()+
+#  scale_color_manual(values=c("#CC6666", "black"))+
+  facet_wrap(vars(Survey))
+
+    labs(color="Assessment year",linetype="Assessment year")+
+  ylab("All Landings")+
+  theme_bw()+
+  theme(legend.position="bottom")
