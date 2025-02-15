1993fee1465ce85906c0502109f6aa7eb49c8647
shcaba <jason.cope@noaa.gov>
2025-02-14 19:12:48 -0800

    update indices section

--- docs/Pre_assessment/plots/Data comp plots.R
+++ docs/Pre_assessment/plots/Data comp plots.R
@@ -52,19 +52,63 @@ ggplot(Catch_comp_in_All,aes(Year,Landings,color=as.factor(Assessment)))+
 Indices_comp_in<-read.csv("C:/Users/Jason.Cope/Documents/Github/REBS-2025/docs/Pre_assessment/plots/Indices_comp.csv")
 Indices_comp_in_WCGBTS<-subset(Indices_comp_in,Survey=="WCGBTS")
 Indices_comp_in_NWFSC<-subset(Indices_comp_in,Survey=="NWFSC_slope")
-Indices_comp_in_WCGBTS<-subset(Indices_comp_in,Survey=="WCGBTS")
-Indices_comp_in_WCGBTS<-subset(Indices_comp_in,Survey=="WCGBTS")
+Indices_comp_in_AFSC_slope<-subset(Indices_comp_in,Survey=="AFSC_slope")
+Indices_comp_in_Tri<-subset(Indices_comp_in,Survey=="Triennial")
 
 ggplot(Indices_comp_in_WCGBTS,aes(as.numeric(Year),as.numeric(Index),color=as.factor(Type)))+
-  geom_point()
+  geom_point(position=position_dodge(width=0.5))+
+  geom_errorbar(aes(ymin=exp(log(as.numeric(Index))-as.numeric(SD_log)*1.96), 
+                    ymax=exp(log(as.numeric(Index))+as.numeric(SD_log)*1.96)),
+                position=position_dodge(width=0.5))+
+  ylim(0,3000)+
+  xlab("Year")+
+  ylab("WCGBTS Index value")+
+  theme_bw()+
+  theme(legend.position="bottom")+
+  labs(color="Survey model")
   
+
 ggplot(Indices_comp_in_NWFSC,aes(as.numeric(Year),as.numeric(Index),color=as.factor(Type)))+
   geom_point(position=position_dodge(width=0.5))+
-  ylim(0,3000)
+  geom_errorbar(aes(ymin=exp(log(as.numeric(Index))-as.numeric(SD_log)*1.96), 
+                    ymax=exp(log(as.numeric(Index))+as.numeric(SD_log)*1.96)),
+                position=position_dodge(width=0.5))+
+  ylim(0,9000)+
+  xlab("Year")+
+  ylab("NWFSC slope Index value")+
+  theme_bw()+
+  theme(legend.position="bottom")+
+  labs(color="Survey model")+
+  scale_x_discrete(breaks=c(1999:2002),labels=c("1999","2000","2001","2002"))
+
+ggplot(Indices_comp_in_AFSC_slope,aes(as.numeric(Year),as.numeric(Index),color=as.factor(Type)))+
+  geom_point(position=position_dodge(width=0.5))+
+  geom_errorbar(aes(ymin=exp(log(as.numeric(Index))-as.numeric(SD_log)*1.96), 
+                    ymax=exp(log(as.numeric(Index))+as.numeric(SD_log)*1.96)),
+                position=position_dodge(width=0.5))+
+  ylim(0,3000)+
+  xlab("Year")+
+  ylab("AFSC slope Index value")+
+  theme_bw()+
+  theme(legend.position="bottom")+
+  labs(color="Survey model")
+
+ggplot(Indices_comp_in_Tri,aes(as.numeric(Year),as.numeric(Index),color=as.factor(Type)))+
+  geom_point(position=position_dodge(width=0.5))+
+  geom_errorbar(aes(ymin=exp(log(as.numeric(Index))-as.numeric(SD_log)*1.96), 
+                    ymax=exp(log(as.numeric(Index))+as.numeric(SD_log)*1.96)),
+                position=position_dodge(width=0.5))+
+  ylim(0,2000)+
+  xlab("Year")+
+  ylab("Triennial Index value")+
+  theme_bw()+
+  theme(legend.position="bottom")+
+  labs(color="Survey model")
+
 
 
 ggplot(Indices_comp_in,aes(as.numeric(Year),as.numeric(Index),color=as.factor(Type)))+
-  geom_point()+
+  geom_point(position=position_dodge(width=0.5))+
 #  scale_color_manual(values=c("#CC6666", "black"))+
   facet_wrap(vars(Survey))
 
