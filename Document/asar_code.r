d38341d2d3882721af61832c19a2fd05f8cfae11
shcaba <jason.cope@noaa.gov>
2025-02-20 13:52:04 -0800

    update Document files

--- Document/asar_code.r
+++ Document/asar_code.r
@@ -4,10 +4,10 @@ library(satf)
 
 asar::convert_output(
   output_file = "Report.sso",
-  outdir = "C:/Users/Jason.Cope/Documents/Github/REBS-2025/Document",
+  outdir = "C:/Users/Jason.Cope/Documents/Github/REBS-2025/Document/update",
   model = "ss3",
   file_save = TRUE,
-  savedir = "C:/Users/Jason.Cope/Documents/Github/REBS-2025/Document",
+  savedir = "C:/Users/Jason.Cope/Documents/Github/REBS-2025/Document/update",
   save_name = "reye_blkspt_convert_output")
 
 asar::create_template(
@@ -18,13 +18,13 @@ asar::create_template(
   complex=TRUE,
   spp_latin = "Sebastes aleutianus",
   year = 2025,
-  file_dir = "C:/Users/Jason.Cope/Documents/Github/REBS-2025/Document/",
+  file_dir = "C:/Users/Jason.Cope/Documents/Github/REBS-2025/Document/update/",
   author = c("Jason M. Cope", "Vladlena Gertseva", "Fabio Caltabellotta", "Claire Rosemond", "Alison D. Whitman"),
   include_affiliation = TRUE,
   simple_affiliation = FALSE,
   #param_names = c("nf","sf"),
-  #param_values = c("Trawl", "Hook-and_line","At-sea Hake","Foreign catches"),
-  resdir = "C:/Users/Jason.Cope/Documents/Github/REBS-2025/Document",
+  #param_values = c("Trawl", "Hook-and_line","At-sea Hake"),
+  resdir = "C:/Users/Jason.Cope/Documents/Github/REBS-2025/Document/update/",
   model_results = "reye_blkspt_convert_output.csv",
   #make_rda = TRUE, 
   rda_dir = getwd(),
