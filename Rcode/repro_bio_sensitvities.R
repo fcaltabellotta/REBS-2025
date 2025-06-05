48fde3acaea2670eafdc1906b109492d9cc6d138
rclairer <claire.rosemond@noaa.gov>
2025-06-05 15:03:06 -0700

    run base model and new models folder

---
+++ Rcode/repro_bio_sensitvities.R
@@ -0,0 +1,44 @@
+library(here)
+library(r4ss)
+library(dplyr)
+library(purrr)
+library(furrr)
+library(ggplot2)
+
+
+#run base model
+
+base_model_dir <- file.path(here::here('models','base_model'))
+
+rcr_base_model_dir <- file.path(here::here('models','rcr_base_model'))
+
+copy_SS_inputs(
+  dir.old = base_model_dir, 
+  dir.new = rcr_base_model_dir,
+  create.dir = TRUE,
+  overwrite = TRUE,
+  use_ss_new = FALSE,
+  verbose = TRUE
+)
+
+rcr_base_model <- r4ss::SS_read(here::here('models','rcr_base_model'))
+
+r4ss::get_ss3_exe(dir = rcr_base_model_dir)
+
+r4ss::run(dir = rcr_base_model_dir, show_in_console = TRUE, extras = "-nohess")
+
+replist <- SS_output(dir = rcr_base_model_dir)
+
+SS_plots(replist)
+
+##############################################################
+
+model_directory <- here::here(
+  'models')
+base_model_name <- here::here(
+  'models',
+  'rcr_base_model'
+)
+exe_loc <- here::here('models/rcr_base_model/ss3')
+base_model <- SS_read(base_model_name, ss_new = TRUE)
+base_out <- SS_output(base_model_name)
